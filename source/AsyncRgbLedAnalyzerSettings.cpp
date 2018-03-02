f4f06deb09a91ac4e96a2d9be6ceba9e7a472657
James Turner <james.turner@kdab.com>
2018-03-02 15:46:44 +0000

    Revised settings after discussion with Mark

--- source/AsyncRgbLedAnalyzerSettings.cpp
+++ source/AsyncRgbLedAnalyzerSettings.cpp
@@ -5,20 +5,20 @@ const char* DEFAULT_CHANNEL_NAME = "Addressable LEDs (Async)";
 
 AsyncRgbLedAnalyzerSettings::AsyncRgbLedAnalyzerSettings()
 {
+    InitControllerData();
+
 	mInputChannelInterface.reset( new AnalyzerSettingInterfaceChannel() );
 	mInputChannelInterface->SetTitleAndTooltip( "LED Channel", "Standard Addressable LEDs (Async)" );
 	mInputChannelInterface->SetChannel( mInputChannel );
 
 	mControllerInterface.reset( new AnalyzerSettingInterfaceNumberList() );
 	mControllerInterface->SetTitleAndTooltip( "LED Controller", "Specify the LED controller in use." );
-	mControllerInterface->AddNumber( LED_WS2811, "WS2811", "Worldsemi 24-bit RGB controller" );
-	mControllerInterface->AddNumber( LED_WS2812B, "WS2812B", "Worldsemi 24-bit RGB controller" );
-	mControllerInterface->AddNumber( LED_WS2813, "WS2813", "Worldsemi 24-bit RGB integrated light-source" );
-	mControllerInterface->AddNumber( LED_TM1809, "TM1809", "Titan Micro 9-chanel 24-bit RGB controller" );
-	mControllerInterface->AddNumber( LED_TM1804, "TM1804", "Titan Micro 24-bit RGB controller" );
-	mControllerInterface->AddNumber( LED_UCS1903, "UCS1903", "UCS1903 24-bit RGB controller" );
-	mControllerInterface->AddNumber( LED_LPD1886_8bit, "LPD1886 - 24bit", "LPD1886 RGB controller in 24-bit mode" );
-	mControllerInterface->AddNumber( LED_LPD1886_12bit, "LPD1886 - 36bit", "LPD1886 RGB controller in 36-bit mode" );
+
+    int index = 0;
+    for (const auto& controllerData : mControllers) {
+        mControllerInterface->AddNumber(index++, controllerData.mName.c_str(),
+                                        controllerData.mDescription.c_str());
+    }
 
 	mControllerInterface->SetNumber( mLEDController );
 
@@ -35,6 +35,36 @@ AsyncRgbLedAnalyzerSettings::AsyncRgbLedAnalyzerSettings()
 
 AsyncRgbLedAnalyzerSettings::~AsyncRgbLedAnalyzerSettings()
 {
+}
+
+void AsyncRgbLedAnalyzerSettings::InitControllerData()
+{
+    // order of values here must correspond to the Controller enum
+    mControllers = {
+        // name, description, bits per channel, channels per frame, reset time nsec, low-speed data nsec, has high speed, high speed data nsec, color layout
+
+        // https://cdn-shop.adafruit.com/datasheets/WS2811.pdf
+        {"WS2811", "Worldsemi 24-bit RGB controller", 8, 3, 50000, {{500, 2000}, {1200, 1300}}, true, {{250, 1000}, {600, 650}}, LAYOUT_RGB},
+
+        // https://cdn-shop.adafruit.com/datasheets/WS2812B.pdf
+        {"WS2812B", "Worldsemi 24-bit RGB integrated light-source", 8, 3, 50000, {{400, 850}, {800, 450}}, false, {{0, 0}, {0, 0}}, LAYOUT_GRB},
+
+        // http://www.led-color.com/upload/201609/WS2813%20LED.pdf
+        {"WS2813", "Worldsemi 24-bit RGB integrated light-source", 8, 3, 300000, {{375, 200}, {875, 200}}, false, {{0, 0}, {0, 0}}, LAYOUT_GRB},
+
+        // https://www.deskontrol.net/descargas/datasheets/TM1809.pdf
+        {"TM1809", "Titan Micro 9-chanel 24-bit RGB controller", 8, 9, 24000, {{600, 1200}, {1200, 600}}, true, {{320, 600}, {600, 320}}, LAYOUT_RGB},
+
+        // https://www.deskontrol.net/descargas/datasheets/TM1804.pdf
+        {"TM1804", "Titan Micro 24-bit RGB controller", 8, 3, 10000, {{1000, 2000}, {2000, 1000}}, false, {{0, 0}, {0, 0}}, LAYOUT_RGB},
+
+        // http://www.bestlightingbuy.com/pdf/UCS1903%20datasheet.pdf
+        {"UCS1903", "UCS1903 24-bit RGB controller", 8, 3, 24000, {{500, 2000}, {2000, 500}}, true, {{250, 1000}, {1000, 250}}, LAYOUT_RGB},
+
+        // https://www.syncrolight.co.uk/datasheets/LPD1886%20datasheet.pdf
+        {"LPD1886 - 24 bit", "LPD1886 RGB controller in 24-bit mode", 8, 3, 24000, {{200, 600}, {600, 200}}, false, {{0, 0}, {0, 0}}, LAYOUT_RGB},
+        {"LPD1886 - 36 bit", "LPD1886 RGB controller in 36-bit mode", 12, 3, 24000, {{200, 600}, {600, 200}}, false, {{0, 0}, {0, 0}}, LAYOUT_RGB}
+    };
 }
 
 bool AsyncRgbLedAnalyzerSettings::SetSettingsFromInterfaces()
@@ -80,30 +110,41 @@ const char* AsyncRgbLedAnalyzerSettings::SaveSettings()
 	return SetReturnString( text_archive.GetString() );
 }
 
-
 U8 AsyncRgbLedAnalyzerSettings::BitSize() const
 {
-	switch (mLEDController) {
-	case LED_LPD1886_12bit:
-		return 12;
-	default:
-		return 8;
-	}
+    return mControllers.at(mLEDController).mBitsPerChannel;
 }
 
 U8 AsyncRgbLedAnalyzerSettings::LEDChannelCount() const
 {
-	switch (mLEDController) {
-	case LED_WS2811:
-	case LED_WS2812B:
-	case LED_WS2813:
-	case LED_TM1804:
-	case LED_UCS1903:
-	case LED_LPD1886_8bit:
-	case LED_LPD1886_12bit:
-		return 3;
+    return mControllers.at(mLEDController).mChannelCount;
+}
 
-	case LED_TM1809:
-		return 9;
-	}
+U32 AsyncRgbLedAnalyzerSettings::DataTimeNSecHigh(BitState value) const
+{
+    const auto& d = mControllers.at(mLEDController);
+    return mIsHighSpeedMode ? d.mDataTimingHighSpeedNsec[value][0]
+                            : d.mDataTimingNSec[value][1];
+}
+
+U32 AsyncRgbLedAnalyzerSettings::DataTimeNSecLow(BitState value) const
+{
+    const auto& d = mControllers.at(mLEDController);
+    return mIsHighSpeedMode ? d.mDataTimingHighSpeedNsec[value][0]
+                            : d.mDataTimingNSec[value][1];
+}
+
+bool AsyncRgbLedAnalyzerSettings::IsHighSpeedSupported() const
+    {
+    return mControllers.at(mLEDController).mHasHighSpeed;
+}
+
+U32 AsyncRgbLedAnalyzerSettings::ResetTimeNSec() const
+{
+    return mControllers.at(mLEDController).mResetTimeNSec;
+}
+
+auto AsyncRgbLedAnalyzerSettings::GetColorLayout() const -> ColorLayout
+{
+    return mControllers.at(mLEDController).mLayout;
 }
--- source/AsyncRgbLedAnalyzerSettings.h
+++ source/AsyncRgbLedAnalyzerSettings.h
@@ -1,6 +1,8 @@
 #ifndef ASYNCRGBLED_ANALYZER_SETTINGS
 #define ASYNCRGBLED_ANALYZER_SETTINGS
 
+#include <vector>
+
 #include <AnalyzerSettings.h>
 #include <AnalyzerTypes.h>
 
@@ -36,14 +38,58 @@ public:
 	Controller mLEDController = LED_WS2811;
 	Channel mInputChannel = UNDEFINED_CHANNEL;
 
-	/// bits ber LED channel, either 8 or 10 at present
+    /// bits ber LED channel, either 8 or 12 at present
 	U8 BitSize() const; 
 
 	/// LED channel count, either 3 (RGB) or 9 (three RGB outputs) at present
 	U8 LEDChannelCount() const;
+
+	/// interval to consider a reset rather than a data value
+
+    void SetHighSpeedMode(bool isHighSpeed);
+
+    bool IsHighSpeedSupported() const;
+
+    U32 DataTimeNSecHigh(BitState value) const;
+
+    U32 DataTimeNSecLow(BitState value) const;
+
+    U32 ResetTimeNSec() const;
+
+    enum ColorLayout {
+        LAYOUT_RGB = 0,
+        LAYOUT_GRB
+    };
+
+    ColorLayout GetColorLayout() const;
+
 protected:
+    void InitControllerData();
+
 	std::unique_ptr< AnalyzerSettingInterfaceChannel >	mInputChannelInterface;
 	std::unique_ptr< AnalyzerSettingInterfaceNumberList >	mControllerInterface;
+
+    // we can't do direct defualt initialisation here, since according to C++11
+    // that makes this type non-POD and hence unsuitable for direct initialisation.
+    // C++14 fixes this.
+    struct LedControllerData
+    {
+        std::string mName;
+        std::string mDescription;
+        U8 mBitsPerChannel; // = 8;
+        U8 mChannelCount; // = 3;
+        U32 mResetTimeNSec;
+        U32 mDataTimingNSec[2][2];
+
+        bool mHasHighSpeed; // = trye
+        U32 mDataTimingHighSpeedNsec[2][2];
+
+        AsyncRgbLedAnalyzerSettings::ColorLayout mLayout; // = LAYOUT_RGB
+    };
+
+    std::vector<LedControllerData> mControllers;
+
+    bool mIsHighSpeedMode = false;
 };
 
 #endif //ASYNCRGBLED_ANALYZER_SETTINGS
