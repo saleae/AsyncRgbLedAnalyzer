99c96b40888e7f8c1418f04cdcd695cc0e9c08c8
James Turner <james.turner@kdab.com>
2018-03-20 16:53:52 +0000

    AStyle changes

--- source/AsyncRgbLedAnalyzerResults.cpp
+++ source/AsyncRgbLedAnalyzerResults.cpp
@@ -6,9 +6,9 @@
 #include <fstream>
 
 AsyncRgbLedAnalyzerResults::AsyncRgbLedAnalyzerResults( AsyncRgbLedAnalyzer* analyzer, AsyncRgbLedAnalyzerSettings* settings )
-:	AnalyzerResults(),
-	mSettings( settings ),
-	mAnalyzer( analyzer )
+    :   AnalyzerResults(),
+        mSettings( settings ),
+        mAnalyzer( analyzer )
 {
 }
 
@@ -16,7 +16,7 @@ AsyncRgbLedAnalyzerResults::~AsyncRgbLedAnalyzerResults()
 {
 }
 
-void AsyncRgbLedAnalyzerResults::GenerateRGBStrings(const RGBValue& rgb, DisplayBase base, size_t bufSize, char* redBuf, char* greenBuff, char* blueBuf)
+void AsyncRgbLedAnalyzerResults::GenerateRGBStrings( const RGBValue& rgb, DisplayBase base, size_t bufSize, char* redBuf, char* greenBuff, char* blueBuf )
 {
     // generate a numerical representation of each color channel,
     // respecting the display-base setting
@@ -27,39 +27,39 @@ void AsyncRgbLedAnalyzerResults::GenerateRGBStrings(const RGBValue& rgb, Display
 
 void AsyncRgbLedAnalyzerResults::GenerateBubbleText( U64 frame_index, Channel& channel, DisplayBase display_base )
 {
-	ClearResultStrings();
-	Frame frame = GetFrame( frame_index );
+    ClearResultStrings();
+    Frame frame = GetFrame( frame_index );
 
-	U32 ledIndex = frame.mData2;
-    RGBValue rgb = RGBValue::CreateFromU64(frame.mData1);
+    U32 ledIndex = frame.mData2;
+    RGBValue rgb = RGBValue::CreateFromU64( frame.mData1 );
 
     // generate a Web/CSS representation of the color value
     U8 webColor[3];
-    rgb.ConvertTo8Bit(mSettings->BitSize(), webColor);
+    rgb.ConvertTo8Bit( mSettings->BitSize(), webColor );
     char webBuf[8];
-    ::snprintf(webBuf, sizeof(webBuf), "#%02x%02x%02x", webColor[0], webColor[1], webColor[2]);
+    ::snprintf( webBuf, sizeof( webBuf ), "#%02x%02x%02x", webColor[0], webColor[1], webColor[2] );
 
     const int colorNumericBufferLength = 16;
     char redString[colorNumericBufferLength],
-            greenString[colorNumericBufferLength],
-            blueString[colorNumericBufferLength];
+         greenString[colorNumericBufferLength],
+         blueString[colorNumericBufferLength];
 
-   GenerateRGBStrings(rgb, display_base, colorNumericBufferLength, redString, greenString, blueString);
+    GenerateRGBStrings( rgb, display_base, colorNumericBufferLength, redString, greenString, blueString );
 
-// generate four different string variants of varying length, starting with
-// the longest and decreasing in size
+    // generate four different string variants of varying length, starting with
+    // the longest and decreasing in size
     char buf[256];
 
     // example: LED: 13 Red: 0x1A Green: 0x2B Blue: 0x3C #1A2B3C
-    ::snprintf(buf, sizeof(buf), "LED %d Red: %s Green: %s Blue: %s %s", ledIndex, redString, greenString, blueString, webBuf);
+    ::snprintf( buf, sizeof( buf ), "LED %d Red: %s Green: %s Blue: %s %s", ledIndex, redString, greenString, blueString, webBuf );
     AddResultString( buf );
 
     // example: 13 R:0x1A G:0x2B B:0x3C #1A2B3C
-    ::snprintf(buf, sizeof(buf), "%d R: %s G: %s B: %s %s", ledIndex, redString, greenString, blueString, webBuf);
+    ::snprintf( buf, sizeof( buf ), "%d R: %s G: %s B: %s %s", ledIndex, redString, greenString, blueString, webBuf );
     AddResultString( buf );
 
     // example: (13) #1A2B3C
-    ::snprintf(buf, sizeof(buf), "(%d) %s", ledIndex, webBuf);
+    ::snprintf( buf, sizeof( buf ), "(%d) %s", ledIndex, webBuf );
 
     // example: #1A2B3C
     AddResultString( webBuf );
@@ -67,37 +67,40 @@ void AsyncRgbLedAnalyzerResults::GenerateBubbleText( U64 frame_index, Channel& c
 
 void AsyncRgbLedAnalyzerResults::GenerateExportFile( const char* file, DisplayBase display_base, U32 export_type_user_id )
 {
-	std::ofstream file_stream( file, std::ios::out );
+    std::ofstream file_stream( file, std::ios::out );
 
-	U64 trigger_sample = mAnalyzer->GetTriggerSample();
-	U32 sample_rate = mAnalyzer->GetSampleRate();
+    U64 trigger_sample = mAnalyzer->GetTriggerSample();
+    U32 sample_rate = mAnalyzer->GetSampleRate();
 
     file_stream << "Time [s], Packet ID, LED Index, Red, Green, Blue, Web-CSS" << std::endl;
 
     const U64 num_frames = GetNumFrames();
-    for( U64 i=0; i < num_frames; i++ )
-	{
+
+    for ( U64 i = 0; i < num_frames; i++ )
+    {
         const Frame frame = GetFrame( i );
-        U64 packetId = GetPacketContainingFrameSequential(num_frames);
-        if (packetId == INVALID_RESULT_INDEX) {
+        U64 packetId = GetPacketContainingFrameSequential( num_frames );
+
+        if ( packetId == INVALID_RESULT_INDEX )
+        {
             packetId = -1;
         }
 
-		char time_str[128];
-		AnalyzerHelpers::GetTimeString( frame.mStartingSampleInclusive, trigger_sample, sample_rate, time_str, 128 );
+        char time_str[128];
+        AnalyzerHelpers::GetTimeString( frame.mStartingSampleInclusive, trigger_sample, sample_rate, time_str, 128 );
 
-        RGBValue rgb = RGBValue::CreateFromU64(frame.mData1);
+        RGBValue rgb = RGBValue::CreateFromU64( frame.mData1 );
 
         // RGB numerical value representation
         const size_t bufSize = 16;
         char rs[bufSize], gs[bufSize], bs[bufSize];
-        GenerateRGBStrings(rgb, display_base, bufSize, rs, gs, bs);
+        GenerateRGBStrings( rgb, display_base, bufSize, rs, gs, bs );
 
         // CSS representation
         U8 webColor[3];
-        rgb.ConvertTo8Bit(mSettings->BitSize(), webColor);
+        rgb.ConvertTo8Bit( mSettings->BitSize(), webColor );
         char webBuf[8];
-        ::snprintf(webBuf, sizeof(webBuf), "#%02x%02x%02x", webColor[0], webColor[1], webColor[2]);
+        ::snprintf( webBuf, sizeof( webBuf ), "#%02x%02x%02x", webColor[0], webColor[1], webColor[2] );
 
         file_stream << time_str << "," << packetId << "," << frame.mData2 << ","
                     << rs << ","
@@ -105,46 +108,46 @@ void AsyncRgbLedAnalyzerResults::GenerateExportFile( const char* file, DisplayBa
                     << bs << ","
                     << webBuf << std::endl;
 
-		if( UpdateExportProgressAndCheckForCancel( i, num_frames ) == true )
-		{
-			file_stream.close();
-			return;
-		}
-	}
+        if ( UpdateExportProgressAndCheckForCancel( i, num_frames ) == true )
+        {
+            file_stream.close();
+            return;
+        }
+    }
 
-	file_stream.close();
+    file_stream.close();
 }
 
 void AsyncRgbLedAnalyzerResults::GenerateFrameTabularText( U64 frame_index, DisplayBase display_base )
 {
 #ifdef SUPPORTS_PROTOCOL_SEARCH
-	Frame frame = GetFrame( frame_index );
-	ClearTabularText();
+    Frame frame = GetFrame( frame_index );
+    ClearTabularText();
 
     const U32 ledIndex = frame.mData2;
-    const RGBValue rgb = RGBValue::CreateFromU64(frame.mData1);
+    const RGBValue rgb = RGBValue::CreateFromU64( frame.mData1 );
 
     const int colorNumericBufferLength = 8;
     char redString[colorNumericBufferLength],
-            greenString[colorNumericBufferLength],
-            blueString[colorNumericBufferLength];
+         greenString[colorNumericBufferLength],
+         blueString[colorNumericBufferLength];
 
-   GenerateRGBStrings(rgb, display_base, colorNumericBufferLength, redString, greenString, blueString);
+    GenerateRGBStrings( rgb, display_base, colorNumericBufferLength, redString, greenString, blueString );
 
-   // target content: [13] 0x1A, 0x2B, 0x3C
-   char buf[64];
-   ::snprintf(buf, 64, "[%d] %s, %s, %s", ledIndex, redString, greenString, blueString);
+    // target content: [13] 0x1A, 0x2B, 0x3C
+    char buf[64];
+    ::snprintf( buf, 64, "[%d] %s, %s, %s", ledIndex, redString, greenString, blueString );
     AddTabularText( buf );
 #endif
 }
 
 void AsyncRgbLedAnalyzerResults::GeneratePacketTabularText( U64 packet_id, DisplayBase display_base )
 {
-	//not supported
+    //not supported
 
 }
 
 void AsyncRgbLedAnalyzerResults::GenerateTransactionTabularText( U64 transaction_id, DisplayBase display_base )
 {
-	//not supported
+    //not supported
 }
--- source/AsyncRgbLedHelpers.h
+++ source/AsyncRgbLedHelpers.h
@@ -3,7 +3,8 @@
 
 #include <AnalyzerTypes.h>
 
-enum ColorLayout {
+enum ColorLayout
+{
     LAYOUT_RGB = 0,
     LAYOUT_GRB
 };
@@ -13,19 +14,19 @@ struct RGBValue
     RGBValue() = default;
     ~RGBValue() = default;
 
-    RGBValue(U16 r, U16 g, U16 b) :
-        red(r), green(g), blue(b) {;}
+    RGBValue( U16 r, U16 g, U16 b ) :
+        red( r ), green( g ), blue( b ) {;}
 
     U16 red = 0;
     U16 green = 0;
     U16 blue = 0;
     U16 padding = 0; // no alpha in LED colors
 
-    void ConvertToControllerOrder(ColorLayout layout, U16* values) const;
+    void ConvertToControllerOrder( ColorLayout layout, U16* values ) const;
 
-    static RGBValue CreateFromControllerOrder(ColorLayout layout, U16* values);
+    static RGBValue CreateFromControllerOrder( ColorLayout layout, U16* values );
 
-    static RGBValue CreateFromU64(U64 raw);
+    static RGBValue CreateFromU64( U64 raw );
 
     U64 ConvertToU64() const;
 
@@ -35,36 +36,36 @@ struct RGBValue
      * @param bitSize - bits used in this RGB value, eg 8, 10, 12 or 16
      * @param values - array of three U8s to store output web color value
      */
-    void ConvertTo8Bit(U8 bitSize, U8* values) const;
+    void ConvertTo8Bit( U8 bitSize, U8* values ) const;
 };
 
 struct TimingTolerance
 {
     TimingTolerance() = default;
 
-    TimingTolerance(double minS, double nomS, double maxS) :
-        mMinimumSec(minS), mNominalSec(nomS), mMaximumSec(maxS)
+    TimingTolerance( double minS, double nomS, double maxS ) :
+        mMinimumSec( minS ), mNominalSec( nomS ), mMaximumSec( maxS )
     {;}
 
     double mMinimumSec = 0.0;
     double mNominalSec = 0.0;
     double mMaximumSec = 0.0;
 
-    bool WithinTolerance(const double t) const;
+    bool WithinTolerance( const double t ) const;
 };
 
 struct BitTiming
 {
     BitTiming() {;}
 
-    BitTiming(const TimingTolerance& pt, const TimingTolerance& nt) :
-        mPositiveTiming(pt), mNegativeTiming(nt)
+    BitTiming( const TimingTolerance& pt, const TimingTolerance& nt ) :
+        mPositiveTiming( pt ), mNegativeTiming( nt )
     {;}
 
     TimingTolerance mPositiveTiming;
     TimingTolerance mNegativeTiming;
 
-    bool WithinTolerance(const double positiveTime, const double negativeTime) const;
+    bool WithinTolerance( const double positiveTime, const double negativeTime ) const;
 };
 
 #endif // of #define ASYNCRGBLED_ANALYZER_SETTINGS
